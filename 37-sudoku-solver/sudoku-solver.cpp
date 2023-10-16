bb45d93981052b64a271f7e8c5bef508db628162
amank182407 <76875000+amank182407@users.noreply.github.com>
2023-10-16 20:07:39 +0530

    Time: 29 ms (21.64%) | Memory: 6.7 MB (48.47%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+        
+    }
+    bool solve(vector<vector<char>>& board)
+    {
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[0].size();j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(issafe(board,i,j,c))
+                        {
+                            board[i][j]=c;
+
+                            bool nextsol=solve(board);
+                            if(nextsol)
+                            return true;
+                            else
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool issafe(vector<vector<char>>& board,int row,int col,char c)
+    {
+        for(int i=0;i<9;i++)
+        {   //row check
+            if(board[row][i]==c)
+            return false;
+
+            //column check
+            if(board[i][col]==c)
+            return false;
+
+            //3*3 matrix
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+            return false;
+        }
+        return true;
+    }
+};
