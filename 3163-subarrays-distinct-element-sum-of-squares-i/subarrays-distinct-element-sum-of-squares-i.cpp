a766fbb37ec0c6894530b39eaf99b9e7ae56ff08
amank182407 <76875000+amank182407@users.noreply.github.com>
2023-10-28 21:37:34 +0530

    Time: 41 ms (30.00%) | Memory: 32.6 MB (20.00%) - LeetSync

--- 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.cpp
+++ 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
    
     
-    int sumCounts(vector<int>& nums) {
-        int sum=0;
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++)
-        {   
-            for(int j=i;j<n;j++)
-            {   set<int> hashset;
-                for(int k=i;k<=j;k++)
-                {
-                hashset.insert(nums[k]);
-                }
-            int n=hashset.size();
-            sum=sum+n*n;
-            } 
+   int sumCounts(vector<int>& nums) {
+    int ans = 0, count = 0;
+    for(int i = 0; i < nums.size(); ++i){
+        unordered_map<int, int> ump; count = 0;
+        for(int j = i; j < nums.size(); ++j){
+            ump[nums[j]]++;
+            if(ump[nums[j]] == 1) count++;
+            ans += count*count;
         }
-        return sum;
-        
     }
+    return ans;
+}
 };
