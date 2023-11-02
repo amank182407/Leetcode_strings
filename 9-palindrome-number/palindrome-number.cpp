2a699eceeb75f69c7dfcaddf161fce10c1ed5ec8
amank182407 <76875000+amank182407@users.noreply.github.com>
2023-11-02 22:21:14 +0530

    Time: 8 ms (54.61%) | Memory: 6.4 MB (32.49%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,7 +4,7 @@ public:
         string s=to_string(x);
         int start=0;
         int end=s.length()-1;
-        while(start<=end)
+        while(start<end)
         {
             if(s[start++]!=s[end--])
             return false;
