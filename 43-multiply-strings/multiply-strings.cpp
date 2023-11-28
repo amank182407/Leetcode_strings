1d54fb970714cd9532fd668dadfbbbee2d61ca7a
amank182407 <76875000+amank182407@users.noreply.github.com>
2023-11-28 23:18:44 +0530

    Time: 11 ms (20.28%) | Memory: 8.5 MB (20.44%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1=="0" or num2=="0") return "0";
+        vector<int> num_1,num_2;
+        for(int i=num1.size()-1;i>=0;i--) num_1.push_back(num1[i]-'0');
+        for(int i=num2.size()-1;i>=0;i--) num_2.push_back(num2[i]-'0');
+        return multiplyH(num_1,num_2);
+    }
+
+    string multiplyH(vector<int> num1,vector<int> num2){
+        
+        vector<int> res;
+        auto c=0;
+        for(int k=0;k<=num1.size()+num2.size()-2;k++){
+            auto s=0;
+            for(int i=0;i<=k;i++){
+                if (i<num1.size() and k-i<num2.size()){
+                    s+= num1[i]*num2[k-i];
+                }
+                
+            }
+            res.push_back((s+c)%10);
+            c=(s+c)/10;
+        }
+        while (c){
+            res.push_back(c%10);
+            c=c/10;
+        }
+        string ress="";
+        for (int i=0;i<res.size();i++) ress=to_string(res[i])+ress;
+        return ress;
+    }
+};
