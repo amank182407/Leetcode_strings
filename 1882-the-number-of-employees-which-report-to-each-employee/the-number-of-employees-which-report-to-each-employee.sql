1c3da8e85469696b1b6a34b36a6c1d00f7767e5d
amank182407 <76875000+amank182407@users.noreply.github.com>
2024-04-16 13:29:16 +0530

    Time: 1689 ms (12.56%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,2 +1,7 @@
-# Write your MySQL query statement below
-select e.employee_id,e.name,count(p.reports_to) as reports_count,round(Avg(p.age),0) as average_age from employees as e left join employees as p on e.employee_id=p.reports_to group by e.employee_id having count(p.reports_to)>=1 order by e.employee_id;
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
+
+# Do upvote if you like the solution
